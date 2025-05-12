e6da540bf3e475f0dbd2806a203f610d1b272b7f
Molochnyy <153619807+Molochnyy@users.noreply.github.com>
2025-05-12 18:12:52 +0300

    Д.З.

--- js/task-1.js
+++ js/task-1.js
@@ -1,30 +1,8 @@
-const customer = {
-    username: "Mango",
-    balance: 24000,
-    discount: 0.1,
-    orders: ["Burger", "Pizza", "Salad"],
-    // Change code below this line
-    getBalance() {
-        return this.balance;
-    },
-    getDiscount() {
-        return this.discount;
-    },
-    setDiscount(value) {
-        this.discount = value;
-    },
-    getOrders() {
-        return this.orders;
-    },
-    addOrder(cost, order) {
-        this.balance -= cost - cost * this.discount;
-        this.orders.push(order);
-    },
-    // Change code above this line
-};
+const list = document.querySelector('#categories');
+console.log(`Number of categories: ${list.children.length}`);
 
-customer.setDiscount(0.15);
-console.log(customer.getDiscount()); // 0.15
-customer.addOrder(5000, "Steak");
-console.log(customer.getBalance()); // 19750
-console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+const items = document.querySelectorAll('.item');
+items.forEach(element => {
+    console.log(`Category: ${element.firstElementChild.textContent}`);
+    console.log(`Elements: ${element.lastElementChild.children.length}`);
+});
